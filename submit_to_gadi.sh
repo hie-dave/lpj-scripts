8f1484d1decc3dd485585bbaf5644eb9fb33422a
Drew Holzworth <drew@holzworth.info>
2022-06-02 14:54:07 +1000

    Eliminate run_lpj script altogether.

--- submit_to_gadi.sh
+++ submit_to_gadi.sh
@@ -1,146 +1,157 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
-# submit.sh
+# Portable bash script to run LPJ-Guess as a parallel job using PBS on Gadi.
 #
-# Portable bash script to run LPJ-GUESS version:
-# BINARY
-# as a parallel job using PBS on Simba.
-#
-# Created automatically on DATE
+# Adapted by Juergen Knauer and Drew Holzworth from older scripts.
 #
 # Usage:
 #
-#   1. Copy script to the directory where you want output written.
-#      This will be called the RUN DIRECTORY.
-#   2. In an editor, set appropriate values for the variables NPROCESS,
-#      INSFILE, GRIDLIST and OUTFILES (NB: no space after the = sign):
-
-NPROCESS=48
-WALLTIME=22:30:00
-MEMORY=96GB
-INSFILE=global_cf.ins
-INPUT_MODULE=cf
-# Read gridlist file name from the .ins file.
-GRIDLIST="$(sed -E -n -e 's/^.*"file_gridlist_cf" \(str "([^"]+)"\)\r?$/\1/p' "${INSFILE}")"
-OUTFILES='*.out'
-QUEUE=normal
-BINARY="guess"
-PROJECT=hw83
-EMAIL=j.knauer@westernsydney.edu.au
-
-#      Where:
-#      NPROCESS     = number of processes in parallel job
-#      WALLTIME     = maximum wall (real) time for job hh:mm:ss
-#      INSFILE      = path to ins file from run directory
-#      INPUT_MODULE = input module to use
-#      GRIDLIST     = path to gridlist file from run directory
-#      OUTFILES     = list of LPJ-GUESS output files in single quotes,
-#                     and separated by spaces (filenames only, including
-#                     extension, no directory.) Shell wildcards are allowed.
+# submit_to_gadi.sh [-n <name>] [-s <file>] [-i <ins-file>]
 #
-#   3. Run the script using the command:
-#        ./submit.sh
-#      or:
-#        ./submit.sh [-n <name>] [-s <file>] [-i <ins-file>]
+# All arguments are optional and interpreted as:
 #
-#      All arguments are optional and interpreted as:
-#      name     = the name of the job (shown in PBS queue)
-#      file     = filename of a file which can override the variables
-#                 above
-#      ins-file = instruction file to use, overrides the INSFILE
-#                 variable above
+#   name     = the name of the job (shown in PBS queue)
+#   file     = filename of a file which can override the input variables. If
+#              set, this file should contain lines of the form VARIABLE=Value
+#   ins-file = instruction file to use, overrides the hardcoded INSFILE
 #
-# Nothing to change past here
-########################################################################
 
-# Exit if any command fails
-set -e
+# Exit immediately if:
+# - Any command fails
+# - Any part of a pipeline fails
+# - We reference any unbound variables
+set -euo pipefail
 
-# Handle the command line arguments
+# Get directory containing this script.
+SUBMIT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd -P )"
+
+# ------------------------------------------------------------------------------
+# Input variables - change these as required.
+# ------------------------------------------------------------------------------
+
+# File paths
+OUT_DIR="/scratch/hw83/jk8585/LPJ_runs" # Outut path.
+CODE_DIR="/home/599/jk8585/LPJ_code/trunk_r8538" # Path to LPJ-Guess repository.
+EXPERIMENT="test_run"               # Experiment name.
+
+# PBS/Cluster Settings
+NPROCESS=48                         # Number of processes in parallel job.
+WALLTIME=22:30:00                   # maximum wall (real) time for job hh:mm:ss
+MEMORY=96GB                         # Total amount of memory available to job.
+QUEUE=normal                        # PBS queue priority(?).
+PROJECT=hw83                        # Project name.
+EMAIL=j.knauer@westernsydney.edu.au # Email address of job owner.
+JOB_NAME=guess                      # Job name (overriden by -n CLI option).
+BINARY="${CODE_DIR}/guess"          # Path to LPJ-Guess binary.
+
+# LPJ-Guess Settings
+INSFILE="${SUBMIT_DIR}/global_cf.ins" # path to ins file from run directory.
+INPUT_MODULE=cf                     # Input module to use.
+OUTFILES='*.out'                    # List of LPJ-Guess output files.
+
+# ------------------------------------------------------------------------------
+# End input variables section.
+# ------------------------------------------------------------------------------
+
+# Parse the command line arguments
 while getopts ":n:s:i:" opt; do
     case $opt in
-	n ) name=$OPTARG ;;
-	s ) submit_vars_file=$OPTARG ;;
-	i ) ins=$OPTARG ;;
+	n ) JOB_NAME=$OPTARG ;;
+	s ) source $OPTARG ;;
+	i ) INSFILE=$OPTARG ;;
     esac
 done
 
-# Override the submit variables with the contents of a file, if given
-if [ -n "$submit_vars_file" ]; then
-    source $submit_vars_file
-fi
-
-# Override INSFILE with the ins-file parameter, if given
-if [ -n "$ins" ]; then
-    INSFILE=$ins
-fi
+# This function will print the absolute path to the given filename.
+# We could use readlink, but it's a Linux tool - not available on MacOS (afaik).
+get_absolute_path() {
+  echo $( cd "$(dirname "$1")"; pwd -P )/$(basename "$1")
+}
 
 # Convert INSFILE to an absolute path since we will be starting the
 # guess instances from different directories.
-# Please note when porting this script: readlink may not be available
-# on non-Linux systems. Also, using absolute path names means the
-# instruction file needs to be in a place accessible from the nodes.
-INSFILE=$(readlink -f "$INSFILE")
+INSFILE="$(get_absolute_path "${INSFILE}")"
 
-GRIDLIST_FILENAME=$(basename $GRIDLIST)
+# Read gridlist file name from the .ins file.
+GRIDLIST="$(sed -E -n -e 's/^.*"file_gridlist_cf" \(str "([^"]+)"\)\r?$/\1/p' "${INSFILE}")"
+GRIDLIST="$(get_absolute_path "${GRIDLIST}")"
+
+# Output path for this run.
+RUN_OUT_DIR="${OUT_DIR}/${EXPERIMENT}"
+
+# Create links and directories.
+mkdir -p "${RUN_OUT_DIR}/output"
+mkdir -p "${RUN_OUT_DIR}/logs"
+
+# Get the file name (without path) of the gridlist file.
+GRIDLIST_FILENAME=$(basename ${GRIDLIST})
 
 # This function creates the gridlist files for each run by splitting
 # the original gridlist file into approximately equal parts.
 function split_gridlist {
-    # Create empty gridlists first to make sure each run gets one
+    # Create empty gridlists first to make sure each run gets one.
     for ((a=1; a <= NPROCESS ; a++))
     do
-      echo > run$a/$GRIDLIST_FILENAME
+        mkdir -p "${RUN_OUT_DIR}/run${a}"
+        echo > "${RUN_OUT_DIR}/run${a}/${GRIDLIST_FILENAME}"
     done
 
     # Figure out suitable number of lines per gridlist, get the number of
     # lines in original gridlist file, divide by NPROCESS and round up.
-    local lines_per_run=$(wc -l $GRIDLIST | \
-	awk '{ x = $1/'$NPROCESS'; d = (x == int(x)) ? x : int(x)+1; print d}')
+    local lines_per_run=$(wc -l ${GRIDLIST} | \
+	awk '{ x = $1/'${NPROCESS}'; d = (x == int(x)) ? x : int(x)+1; print d}')
 
     # Use the split command to split the files into temporary files
-    split --suffix-length=4 --lines $lines_per_run $GRIDLIST tmpSPLITGRID_
+    local tmp_prefix=tmpSPLITGRID_
+    split --suffix-length=4 --lines ${lines_per_run} ${GRIDLIST} "${tmp_prefix}"
 
-    # Move the temporary files into the runX-directories
-    local files=$(ls tmpSPLITGRID_*)
+    # Move the temporary files into the runX-directories.
+    local files="${tmp_prefix}*"
     local i=1
-    for file in $files
+    for file in ${files}
     do
-      mv $file run$i/$GRIDLIST_FILENAME
+      mv ${file} "${RUN_OUT_DIR}/run${i}/${GRIDLIST_FILENAME}"
       i=$((i+1))
     done
 }
 
 # Create header of progress.sh script
+# The progress script will print the progress of each run/process.
+progress_sh="${RUN_OUT_DIR}/progress.sh"
+echo "##############################################################" > "${progress_sh}"
+echo "# PROGRESS.SH" >> "${progress_sh}"
+echo "# Upload current guess.log files from local nodes and check" >> "${progress_sh}"
+echo "# Usage: sh progress.sh" >> "${progress_sh}"
+echo >> "${progress_sh}"
+echo 'DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd -P )"' >> "${progress_sh}"
+echo >> "${progress_sh}"
 
-echo "##############################################################" > progress.sh
-echo "# PROGRESS.SH" >> progress.sh
-echo "# Upload current guess.log files from local nodes and check" >> progress.sh
-echo "# Usage: sh progress.sh" >> progress.sh
-echo >> progress.sh
-
-# Create a run subdirectory for each process and clean up
-
+# Create a run subdirectory for each process, clean up any existing files, and
+# append code to the progress script which reports progress of this run.
 for ((a=1; a <= NPROCESS ; a++))
 do
-  mkdir -p run$a
-  cd run$a ; rm -f guess.log ; rm -f $GRIDLIST_FILENAME ; cd ..
-  echo "echo '********** Last few lines of ./run${a}/guess.log: **********'" >> progress.sh
-  echo "tail ./run${a}/guess.log" >> progress.sh
+    # Ensure the directory exists for this run.
+    mkdir -p "${RUN_OUT_DIR}/run${a}"
+
+    # Delete any existing log files.
+    cd "${RUN_OUT_DIR}/run${a}"; rm -f guess.log; rm -f ${GRIDLIST_FILENAME}; cd ..
+    echo "echo '********** Last few lines of ./run${a}/guess.log: **********'" >> "${progress_sh}"
+    echo "tail \${DIR}/run${a}/guess.log" >> "${progress_sh}"
 done
 
 split_gridlist
 
 # Create PBS script to request place in queue
-cat <<EOF > guess.cmd
+guess_cmd="${RUN_OUT_DIR}/guess.cmd"
+cat <<EOF > "${guess_cmd}"
 #!/bin/bash
-#PBS -l ncpus=$NPROCESS
-#PBS -l walltime=$WALLTIME
-#PBS -l mem=$MEMORY
-#PBS -q $QUEUE
-#PBS -P $PROJECT
+#PBS -l ncpus=${NPROCESS}
+#PBS -l walltime=${WALLTIME}
+#PBS -l mem=${MEMORY}
+#PBS -q ${QUEUE}
+#PBS -P ${PROJECT}
 #PBS -m n
-#PBS -M $EMAIL
+#PBS -M ${EMAIL}
 #PBS -j oe
 #PBS -l wd
 #PBS -W umask=0022
@@ -150,14 +161,15 @@ set -e
 module purge
 module load intel-mpi/2019.6.166
 umask 022
-mpirun ./${BINARY} -parallel -input $INPUT_MODULE $INSFILE
+mpirun ${BINARY} -parallel -input ${INPUT_MODULE} ${INSFILE}
 EOF
 
-cat <<EOF > append.cmd
+append_cmd="${RUN_OUT_DIR}/append.cmd"
+cat <<EOF > "${append_cmd}"
 #!/bin/bash
 #PBS -l ncpus=1
-#PBS -l walltime=$WALLTIME
-#PBS -l mem=$MEMORY
+#PBS -l walltime=${WALLTIME}
+#PBS -l mem=${MEMORY}
 #PBS -q normal
 #PBS -l wd
 #PBS -j oe
@@ -177,18 +189,18 @@ function append_files {
     done
 }
 pushd run1 &> /dev/null
-outfiles_unexpanded='$OUTFILES'
+outfiles_unexpanded='${OUTFILES}'
 outfiles_expanded=\$(echo \$outfiles_unexpanded)
 popd &> /dev/null
 for file in \$outfiles_expanded
 do
-  append_files $NPROCESS \$file
+  append_files ${NPROCESS} \$file
 done
 cat run*/guess.log > guess.log
 EOF
 
 # Submit guess job
-append_dependency=$(qsub -N ${name:-"guess"} guess.cmd)
+append_dependency=$(qsub -N "${JOB_NAME}" "${guess_cmd}")
 
 # Submit append job
-qsub -W depend=afterok:$append_dependency -N ${name:-"guess"}"_append" append.cmd
+qsub -W depend=afterok:${append_dependency} -N "${JOB_NAME}_append" "${append_cmd}"
