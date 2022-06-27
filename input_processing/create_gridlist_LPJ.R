67d9739b261dfa26dd5e31b1e873702d43996774
juergenknauer <J.Knauer@westernsydney.edu.au>
2022-06-27 15:23:10 +1000

    functionality added to create_gridlist file

--- input_processing/create_gridlist_LPJ.R
+++ input_processing/create_gridlist_LPJ.R
@@ -1,10 +1,10 @@
 #!/usr/bin/env Rscript
 
-## Script that creates a gridlist file for LPJ-GUESS
+## Script that creates a soil parameter as well as gridlist file for LPJ-GUESS
 # Juergen Knauer, May 2022
 # Inputs/Settings:
-# 1) Ncdf file containing all soil parameters
-# 2) Spatial extent c(lonmin,lonmax,latmin,latmax) and resolution in deg
+# 1) Ncdf file containing all soil parameters --> soil_file below
+# 2) Spatial extent c(lonmin,lonmax,latmin,latmax) and resolution in deg --> bounding box below
 
 
 library(ncdf4)    # make sure package is installed and path linked in .bashrc file
@@ -13,7 +13,8 @@ options(digits=9)
 ## Settings
 # Soil parameter file (netcdf file containing all required soil input variables)
 soil_file <- "/g/data/hw83/SLG/soilvars_0.1deg.nc"
-outname   <- "LPJ_soilparams.dat"  # name of output file (with extension)
+outname.soilparams  <- "LPJ_soilparams.dat"  # name of soil parameter file (with extension)
+outname.gridlist    <- "CFA_gridlist.txt"    # name of gridlist file (with extension)
 
 ## bounding box
 lonmin <- 140.8
@@ -30,33 +31,39 @@ prec_soilvars <- 3  # precision of soil variables in output file
 sf      <- nc_open(soil_file)
 lat     <- ncvar_get(sf,"lat")
 lon     <- ncvar_get(sf,"lon")
-gridlist_all <- round(expand.grid(lon,lat),prec_lonlat) 
+soilparams_all <- round(expand.grid(lon,lat),prec_lonlat) 
 
 ## get soilvars and add them to gridllist
 for (varname in soilvars){
-   var          <- ncvar_get(sf,varname)
-   gridlist_all <- cbind(gridlist_all,round(c(var),prec_soilvars))
+   var            <- ncvar_get(sf,varname)
+   soilparams_all <- cbind(soilparams_all,round(c(var),prec_soilvars))
 }
 
 # name columns
-colnames(gridlist_all) <- c("lon","lat",soilvars)
+colnames(soilparams_all) <- c("lon","lat",soilvars)
 
 # set all lats and lons outside of bounding box to NAs
-gridlist_all[gridlist_all[,"lon"] > lonmax | gridlist_all[,"lon"] < lonmin & !is.na(gridlist_all[,"lon"]),-c(1,2)] <- NA
-gridlist_all[gridlist_all[,"lat"] > latmax | gridlist_all[,"lat"] < latmin & !is.na(gridlist_all[,"lat"]),-c(1,2)] <- NA
+soilparams_all[soilparams_all[,"lon"] > lonmax | soilparams_all[,"lon"] < lonmin & !is.na(soilparams_all[,"lon"]),-c(1,2)] <- NA
+soilparams_all[soilparams_all[,"lat"] > latmax | soilparams_all[,"lat"] < latmin & !is.na(soilparams_all[,"lat"]),-c(1,2)] <- NA
 
 # exclude all lats and lons with NAs (assumed to be not land)
 # assumes that all soil variables have the same land extent (maybe add check)
-gridlist <- gridlist_all[-which(is.na(gridlist_all[,soilvars[1]])),]
+soilparams <- soilparams_all[-which(is.na(soilparams_all[,soilvars[1]])),]
 
 # account for rounding errors and ensure sum of silt, sand, and clay add up to 100%
-gridlist[,'silt'] <- gridlist[,'silt'] - (apply(gridlist[,c('sand','silt','clay')],1,sum,na.rm=T) - 100)
+soilparams[,'silt'] <- soilparams[,'silt'] - (apply(soilparams[,c('sand','silt','clay')],1,sum,na.rm=T) - 100)
 
 # write to file
-write.table(gridlist,file=outname,quote=F,row.names=F)
+write.table(soilparams,file=outname.soilparams,quote=F,row.names=F)
 
 
 
+
+
+## Now create actual gridlist file (not sure what exact format is required at this point)
+
+gridlist <- data.frame(apply(soilparams,1,function(x) paste0("(",x['lon'],",",x['lat'],")")))
+write.table(gridlist,file=outname.gridlist,quote=F,row.names=F,col.names=F)
 
 
 
