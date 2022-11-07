f031d7c81d9e5f7f3a7c6fbb957ac46ce977b172
Drew Holzworth <drew@holzworth.info>
2022-11-08 09:57:50 +1100

    Cosmetic changes to process_climate_input.sh

--- input_processing/process_climate_input.sh
+++ input_processing/process_climate_input.sh
@@ -40,71 +40,71 @@ lonlatbox="130.0,155.0,-40.0,-20.0"  # format: "lon1,lon2,lat1,lat2"
 for var in $vars ; do
     for scenario in $scenarios ; do
         if [[ "${scenario}" == "historical" ]] ; then
-	    startyear=1951
-	    endyear=2005
-	else
-            startyear=2006
-	    endyear=2099
-	fi
-	mkdir -p ${cf_path_out}/${scenario}/${var}
-	for globmod in $globmods ; do
-	    for regmod in $regmods ; do
-
-		# 1) bring to annual resolution (if necessary)
-		if [[ "${globmod}" == "CSIRO-BOM-ACCESS1-3" && "${scenario}" == "historical" ]] ; then
-                     for ((year=${startyear};year<=${endyear};year++)) ; do
-			 if [[ $(echo "${year}%5" | bc) -eq 1 ]] ; then
-                             syear=${year}
-			     eyear=$(echo "${year} + 4" | bc)
-                             cf_file=${cf_path_in}/${scenario}/${var}/${var}_NARCliMi_${globmod}_${scenario}_r1i1p1_${regmod}_${version}_${freq}_${syear}0101-${eyear}1231.nc
-			     cdo splityear $cf_file ${cf_path_out}/${scenario}/${var}/tmp_
-                         fi
-		     done	     
+			startyear=1951
+			endyear=2005
+		else
+			startyear=2006
+			endyear=2099
 		fi
-		
-	        for ((year=${startyear};year<=${endyear};year++)) ; do
-		    # get filename
-                    if [[ "${globmod}" == "CSIRO-BOM-ACCESS1-3" && "${scenario}" == "historical" ]] ; then
-                        cf_file=${cf_path_out}/${scenario}/${var}/tmp_${year}.nc
-                    else
-                        cf_file=${cf_path_in}/${scenario}/${var}/${var}_NARCliMi_${globmod}_${scenario}_r1i1p1_${regmod}_${version}_${freq}_${year}0101-${year}1231.nc
-		    fi
-		    
-		    # 1) crop file
-		    cdo -s sellonlatbox,${lonlatbox} ${cf_file} tmp.nc
-		
-	            # 2) correct units and change names
-		    case "$var" in
-			hurs)
-			    cdo -s divc,100 tmp.nc tmp1.nc
-			    ncatted -O -a units,${var},o,c,"1" tmp1.nc
-			    mv tmp1.nc tmp.nc
-			;;    
-			#pr)
-			# nothing to be done for pr
-			#;;
-			#tas)
-			# nothing to be done for tas
-			#;;
-			#rsds)
-			# nothing to be done for rsds
-			#;;
-			#sfcWind)
-			# nothing to be done for sfcWind
-			#;;
-		    esac
+		mkdir -p ${cf_path_out}/${scenario}/${var}
+		for globmod in $globmods ; do
+			for regmod in $regmods ; do
 
-		    # 3) rename file and save at final location
-		    mv tmp.nc ${cf_path_out}/${scenario}/${var}/${var}_${scenario}_${globmod}_${regmod}_${freq}_${year}.nc
+				# 1) bring to annual resolution (if necessary)
+				if [[ "${globmod}" == "CSIRO-BOM-ACCESS1-3" && "${scenario}" == "historical" ]] ; then
+					for ((year=${startyear};year<=${endyear};year++)) ; do
+						if [[ $(echo "${year}%5" | bc) -eq 1 ]] ; then
+							syear=${year}
+							eyear=$(echo "${year} + 4" | bc)
+							cf_file=${cf_path_in}/${scenario}/${var}/${var}_NARCliMi_${globmod}_${scenario}_r1i1p1_${regmod}_${version}_${freq}_${syear}0101-${eyear}1231.nc
+							cdo splityear $cf_file ${cf_path_out}/${scenario}/${var}/tmp_
+						fi
+					done
+				fi
 
-		    # cleanup
-		    if [[ -f ${cf_path_out}/${scenario}/${var}/tmp_${year}.nc ]] ; then
-		        rm ${cf_path_out}/${scenario}/${var}/tmp_${year}.nc
-		    fi
-	        done
-	    done
-        done
-    done	
+				for ((year=${startyear};year<=${endyear};year++)) ; do
+					# get filename
+					if [[ "${globmod}" == "CSIRO-BOM-ACCESS1-3" && "${scenario}" == "historical" ]] ; then
+						cf_file=${cf_path_out}/${scenario}/${var}/tmp_${year}.nc
+					else
+						cf_file=${cf_path_in}/${scenario}/${var}/${var}_NARCliMi_${globmod}_${scenario}_r1i1p1_${regmod}_${version}_${freq}_${year}0101-${year}1231.nc
+					fi
+
+					# 1) crop file
+					cdo -s sellonlatbox,${lonlatbox} ${cf_file} tmp.nc
+
+					# 2) correct units and change names
+					case "$var" in
+					hurs)
+						cdo -s divc,100 tmp.nc tmp1.nc
+						ncatted -O -a units,${var},o,c,"1" tmp1.nc
+						mv tmp1.nc tmp.nc
+					;;
+					#pr)
+					# nothing to be done for pr
+					#;;
+					#tas)
+					# nothing to be done for tas
+					#;;
+					#rsds)
+					# nothing to be done for rsds
+					#;;
+					#sfcWind)
+					# nothing to be done for sfcWind
+					#;;
+					esac
+
+					# 3) rename file and save at final location
+					mv tmp.nc ${cf_path_out}/${scenario}/${var}/${var}_${scenario}_${globmod}_${regmod}_${freq}_${year}.nc
+
+					# cleanup
+					if [[ -f ${cf_path_out}/${scenario}/${var}/tmp_${year}.nc ]] ; then
+						rm ${cf_path_out}/${scenario}/${var}/tmp_${year}.nc
+					fi
+				done
+			done
+		done
+	done
 done
 
 # cleanup
