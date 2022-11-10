8dee30492467def35b0b6123d3c83cb124e41e01
Drew Holzworth <drew@holzworth.info>
2022-11-10 11:00:33 +1100

    Updated copy_files.sh to copy to hie-storage
    With overall progress reporting

--- input_processing/copy_files.sh
+++ input_processing/copy_files.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Script to efficiently transfer files from one location on gadi to another
+# location on gadi. In this case applied to transfer NARCliM files to scratch.
 
-## Script to efficiently transfer files from one location on gadi to another location on gadi
-# In this case applied to transfer NARCliM files to scratch
+# Fail immediately with error message if any individual command fails.
+set -euo pipefail
 
 ## PBS settings
 #PBS -N download_clim
@@ -18,7 +20,9 @@
 #PBS -S /bin/bash
 
 basepath_in="/g/data/at43/output/NARCliMi/UNSW"
-basepath_out="/scratch/hw83/jk8585/NARCliM1.5_orig"
+
+dave="/data/hiestorage/WorkingData/MEDLYN_GROUP/PROJECTS/dynamics_simulations"
+dest_dir="/${dave}/narclim/forcing-raw"
 
 globmods="CCCma-CanESM2 CSIRO-BOM-ACCESS1-0 CSIRO-BOM-ACCESS1-3" # global models
 regmods="UNSW-WRF360J UNSW-WRF360K"  # regional (downscaled) climate models 
@@ -27,24 +31,67 @@ vars="hurs pr rsds sfcWind tas tasmin-bc tasmax-bc"  # variables
 version="v1"
 freq="day"
 
-for globmod in $globmods ; do
-    for regmod in $regmods ; do
-	for scenario in $scenarios ; do
-	    for var in $vars ; do
-		# create target directory
-		outpath=${basepath_out}/${scenario}/${var}
-		mkdir -p ${outpath}
+gadi_user=dh7190
+hies_user=u30062639
 
-		# list files to copy
-		cp_files=$(ls ${basepath_in}/${globmod}/${scenario}/r1i1p1/${regmod}/${version}/${freq}/${var}/*.nc)
+src_server="${ssh_user}@gadi-dm.nci.org.au"
+dest_server="${hies_user}@hie-storage.sstars.ws"
+cp_command="rsync -a --partial --info=progress2 --no-i-r"
 
-		# copy files
-		for cp_file in $cp_files ; do
-                    echo $(basename $cp_file)
-		    cp -u $cp_file ${outpath}/$(basename $cp_file)
-		done
-	    done
-	done
-    done
-done
-    
+if ! ssh "${src_server}" echo >/dev/null 2>&1
+then
+	echo "Error: Unable to login to src server ${src_server}."
+	exit 1
+fi
+
+if ! ssh "${src_server}" ssh "${dest_server}" echo >/dev/null 2>&1
+then
+	echo "Error: Cannot connect from ${src_server} to ${dest_server}"
+	exit 1
+fi
+
+# The new method, which recreates the old directory structure, but has the
+# advantage of showing overall progress.
+ssh "${src_server}" "${cp_command}" "${basepath_in}/*" "${dest_dir}/"
+
+# The older method, which creates a nice directory structure in dest. However,
+# this doesn't report overall progress (only per-file).
+# for globmod in $globmods
+# do
+#     for regmod in $regmods
+# 	do
+# 		for scenario in $scenarios
+# 		do
+# 			for var in $vars
+# 			do
+# 				# create target directory
+# 				outpath=${basepath_out}/${scenario}/${var}
+# 				mkdir -p ${outpath}
+
+# 				# list files to copy
+# 				cp_files=$(ls ${basepath_in}/${globmod}/${scenario}/r1i1p1/${regmod}/${version}/${freq}/${var}/*.nc)
+
+# 				# copy files
+# 				for src_file in $cp_files
+# 				do
+# 					# echo $(basename $src_file)
+# 					# cp -u $src_file ${outpath}/$(basename $src_file)
+
+# 					# Get raw filename without path.
+# 					dest_file="$(basename "${src_file}")"
+
+# 					# 
+# 					dest_path="${dest_dir}/${outpath}"
+
+# 					dest="${dest_server}:${dest_path}/${dest_file}"
+
+# 					# Create destination directory if it doesn't already exist.
+# 					ssh "${dest_server}" mkdir -p "${dest_path}"
+
+# 					# Copy the file.
+# 					ssh "${src_server}" "${cp_command}" "${src_file}" "${dest}"
+# 				done
+# 			done
+# 		done
+#     done
+# done
