c7233071428104623b6b1583e9658cbc5eb1fff5
Drew Holzworth <d.holzworth@westernsydney.edu.au>
2026-02-23 15:09:17 +1100

    Added netcdf visualisation script in Julia

--- ncutil/visualise_netcdf.jl
+++ ncutil/visualise_netcdf.jl
@@ -3,8 +3,9 @@
 using ArgParse
 using Dates
 using NCDatasets
-using Plots
+using PlotlyJS
 using Logging
+
 include("nc_helpers.jl")
 
 struct Options
@@ -13,6 +14,9 @@ struct Options
     lat::Float64
     lon::Float64
     log_level::Logging.LogLevel
+    output::Union{Nothing, String}
+    width::Int
+    height::Int
 end
 
 function parse_log_level(level::Int)::Logging.LogLevel
@@ -53,10 +57,25 @@ function parse_cli()::Options
             required = false
             arg_type = Int
             default = 2
+        "-o", "--output"
+            help = "Write plot to file (e.g. out.png, out.pdf). If omitted, show plot interactively."
+            required = false
+            arg_type = String
+        "--width"
+            help = "Width of the output plot (if saving to a raster) in pixels (default: 800)"
+            required = false
+            arg_type = Int
+            default = 800
+        "--height"
+            help = "Height of the output plot (if saving to a raster) in pixels (default: 600)"
+            required = false
+            arg_type = Int
+            default = 600
     end
     p = parse_args(parser)
     level = parse_log_level(p["log-level"])
-    return Options(p["filename"], p["variable"], p["lat"], p["lon"], level)
+    return Options(p["filename"], p["variable"], p["lat"], p["lon"], level,
+                   get(p, "output", nothing), p["width"], p["height"])
 end
 
 function plot_timeseries(ds::NCDataset, variable::String, lat::Float64,
@@ -69,15 +88,25 @@ function plot_timeseries(ds::NCDataset, variable::String, lat::Float64,
     units = haskey(var.attrib, "units") ? var.attrib["units"] : "unknown units"
     ylab = "$variable ($units)"
     title = "Timeseries of $variable at (lat: $alat, lon: $alon)"
-    plot(times, data, xlabel = xlab, ylabel = ylab, title = title)
-    gui()
+    return plot(times, data, Layout(xaxis_title = xlab,
+                                             yaxis_title = ylab,
+                                             title = title))
 end
 
 function main(opts::Options)
     logger = ConsoleLogger(stderr, opts.log_level)
     global_logger(logger)
     NCDataset(opts.filename) do ds
-        plot_timeseries(ds, opts.variable, opts.lat, opts.lon)
+        plt = plot_timeseries(ds, opts.variable, opts.lat, opts.lon)
+        if isnothing(opts.output)
+            @info "Displaying plot interactively"
+            display(plt)
+            println("Press enter to exit...")
+            readline()
+        else
+            @info "Saving plot to $(opts.output)"
+            savefig(plt, opts.output, width = opts.width, height = opts.height)
+        end
     end
 end
 
