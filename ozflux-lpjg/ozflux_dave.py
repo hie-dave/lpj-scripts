2cdc793316082ad08129fa95f06430d72d36fec8
Drew Holzworth <d.holzworth@westernsydney.edu.au>
2026-03-23 14:27:13 +1100

    met-processing pipeline: converted tabs to spaces

--- ozflux-lpjg/ozflux_dave.py
+++ ozflux-lpjg/ozflux_dave.py
@@ -93,81 +93,81 @@ _AMERIFLUX_PRESSURE = "PA_F"
 # Name of the wind speed variable in the ameriflux input files.
 _AMERIFLUX_WIND = "WS_F"
 
-def temp_var(i: str, o: str, aggregator: Callable[[list[float]], float]) \
-	-> ForcingVariable:
-	"""
-	Create a 
-	"""
-	units = _TEMP_UNITS_TRUNK if _TRUNK else _TEMP_UNITS_DAVE
-	return ForcingVariable(i, o, units, aggregator, MIN_TEMP, MAX_TEMP)
+def temp_var(i: str, o: str,
+             aggregator: Callable[[list[float]], float]) -> ForcingVariable:
+    """
+    Create a 
+    """
+    units = _TEMP_UNITS_TRUNK if _TRUNK else _TEMP_UNITS_DAVE
+    return ForcingVariable(i, o, units, aggregator, MIN_TEMP, MAX_TEMP)
 
 def get_ozflux_vars(timestep: int) -> list[ForcingVariable]:
-	"""
-	Get the list of variables required for the daily grass version.
+    """
+    Get the list of variables required for the daily grass version.
 
-	@param timestep: Output timestep in minutes.
-	"""
-	tair = temp_var(IN_TEMP, _OUT_TEMP, numpy.mean)
-	swdown = ForcingVariable(IN_SWDOWN, _OUT_INSOL, _SWDOWN_UNITS, numpy.mean \
-				, MIN_SWDOWN, MAX_SWDOWN)
-	precip = ForcingVariable(IN_PRECLS, _OUT_PREC, _PREC_UNITS, numpy.sum \
-				, MIN_PRECLS, MAX_PRECLS)
-	co2 = ForcingVariable(IN_CO2, _OUT_CO2, _CO2_UNITS, numpy.mean, 250, 900)
-	vpd = ForcingVariable(IN_VPD, _OUT_VPD, _VPD_UNITS, numpy.mean, 0, 100)
-	pressure = ForcingVariable(IN_PS, _OUT_PS, _PS_UNITS, numpy.mean, MIN_PS \
-				, MAX_PS)
-	wind = ForcingVariable(IN_WIND, _OUT_WIND, _WIND_UNITS, numpy.mean, 0, 100)
-	rh = ForcingVariable("RH", "rh", "1", numpy.mean, 0, 1)
+    @param timestep: Output timestep in minutes.
+    """
+    tair = temp_var(IN_TEMP, _OUT_TEMP, numpy.mean)
+    swdown = ForcingVariable(IN_SWDOWN, _OUT_INSOL, _SWDOWN_UNITS, numpy.mean,
+                             MIN_SWDOWN, MAX_SWDOWN)
+    precip = ForcingVariable(IN_PRECLS, _OUT_PREC, _PREC_UNITS, numpy.sum,
+                             MIN_PRECLS, MAX_PRECLS)
+    co2 = ForcingVariable(IN_CO2, _OUT_CO2, _CO2_UNITS, numpy.mean, 250, 900)
+    vpd = ForcingVariable(IN_VPD, _OUT_VPD, _VPD_UNITS, numpy.mean, 0, 100)
+    pressure = ForcingVariable(IN_PS, _OUT_PS, _PS_UNITS, numpy.mean, MIN_PS,
+                               MAX_PS)
+    wind = ForcingVariable(IN_WIND, _OUT_WIND, _WIND_UNITS, numpy.mean, 0, 100)
+    rh = ForcingVariable("RH", "rh", "1", numpy.mean, 0, 1)
 
-	vars = [tair, swdown, precip, co2, vpd, pressure, wind, rh]
+    vars = [tair, swdown, precip, co2, vpd, pressure, wind, rh]
 
-	# If generating a daily file, need to include tmax/tmin variables in output.
-	if timestep / MINUTES_PER_HOUR == 24:
-		vars.append(temp_var(IN_TEMP, _OUT_TMAX, numpy.amax))
-		vars.append(temp_var(IN_TEMP, _OUT_TMIN, numpy.amin))
-		vars.append(ForcingVariable(_IN_RH, _OUT_RH, _RH_OUT_UNITS,
-							  		numpy.mean, 0, 100))
+    # If generating a daily file, need to include tmax/tmin variables in output.
+    if timestep / MINUTES_PER_HOUR == 24:
+        vars.append(temp_var(IN_TEMP, _OUT_TMAX, numpy.amax))
+        vars.append(temp_var(IN_TEMP, _OUT_TMIN, numpy.amin))
+        vars.append(ForcingVariable(_IN_RH, _OUT_RH, _RH_OUT_UNITS,
+                                    numpy.mean, 0, 100))
 
-	return vars
+    return vars
 
 def get_ameriflux_vars(timestep: int) -> list[ForcingVariable]:
-	"""
-	Get the list of variables required for ameriflux processing.
+    """
+    Get the list of variables required for ameriflux processing.
 
-	@param timestep: Output timestep in minutes.
-	"""
-	tair = temp_var(_AMERIFLUX_TAIR, _OUT_TEMP, numpy.mean)
-	swdown = ForcingVariable(_AMERIFLUX_SWDOWN, _OUT_INSOL, _SWDOWN_UNITS
-							 , numpy.mean , MIN_SWDOWN, MAX_SWDOWN)
-	precip = ForcingVariable(_AMERIFLUX_PRECIP, _OUT_PREC, _PREC_UNITS
-						  	 , numpy.sum, MIN_PRECLS, MAX_PRECLS)
-	co2 = ForcingVariable(_AMERIFLUX_CO2, _OUT_CO2, _CO2_UNITS, numpy.mean, 250
-					   		 , 900)
-	vpd = ForcingVariable(_AMERIFLUX_VPD, _OUT_VPD, _VPD_UNITS, numpy.mean, 0
-						     , 100)
-	pressure = ForcingVariable(_AMERIFLUX_PRESSURE, _OUT_PS, _PS_UNITS
-							 , numpy.mean, MIN_PS, MAX_PS)
-	wind = ForcingVariable(_AMERIFLUX_WIND, _OUT_WIND, _WIND_UNITS, numpy.mean
-							 , 0, 100)
+    @param timestep: Output timestep in minutes.
+    """
+    tair = temp_var(_AMERIFLUX_TAIR, _OUT_TEMP, numpy.mean)
+    swdown = ForcingVariable(_AMERIFLUX_SWDOWN, _OUT_INSOL, _SWDOWN_UNITS,
+                             numpy.mean , MIN_SWDOWN, MAX_SWDOWN)
+    precip = ForcingVariable(_AMERIFLUX_PRECIP, _OUT_PREC, _PREC_UNITS,
+                             numpy.sum, MIN_PRECLS, MAX_PRECLS)
+    co2 = ForcingVariable(_AMERIFLUX_CO2, _OUT_CO2, _CO2_UNITS, numpy.mean, 250,
+                          900)
+    vpd = ForcingVariable(_AMERIFLUX_VPD, _OUT_VPD, _VPD_UNITS, numpy.mean, 0,
+                          100)
+    pressure = ForcingVariable(_AMERIFLUX_PRESSURE, _OUT_PS, _PS_UNITS,
+                               numpy.mean, MIN_PS, MAX_PS)
+    wind = ForcingVariable(_AMERIFLUX_WIND, _OUT_WIND, _WIND_UNITS, numpy.mean,
+                           0, 100)
 
-	vars = [tair, swdown, precip, co2, vpd, pressure, wind]
+    vars = [tair, swdown, precip, co2, vpd, pressure, wind]
 
-	# If generating a daily file, need to include tmax/tmin variables in output.
-	if timestep / MINUTES_PER_HOUR == 24:
-		vars.append(temp_var(_AMERIFLUX_TAIR, _OUT_TMAX, numpy.amax))
-		vars.append(temp_var(_AMERIFLUX_TAIR, _OUT_TMIN, numpy.amin))
+    # If generating a daily file, need to include tmax/tmin variables in output.
+    if timestep / MINUTES_PER_HOUR == 24:
+        vars.append(temp_var(_AMERIFLUX_TAIR, _OUT_TMAX, numpy.amax))
+        vars.append(temp_var(_AMERIFLUX_TAIR, _OUT_TMIN, numpy.amin))
 
-	return vars
+    return vars
 
 def write_metadata(nc: Dataset):
-	"""
-	Write metadata specific to the dave-daily-grass version.
+    """
+    Write metadata specific to the dave-daily-grass version.
 
-	@param nc: Output .nc file.
-	"""
-	var_time = nc.variables[DIM_TIME]
-	var_time.calendar = "gregorian"
-	var_time.long_name = DIM_TIME
-	var_time.standard_name = DIM_TIME
-	time_start = DATE_BASELINE.strftime(DATE_FORMAT)
-	var_time.units = "hours since %s" % time_start
+    @param nc: Output .nc file.
+    """
+    var_time = nc.variables[DIM_TIME]
+    var_time.calendar = "gregorian"
+    var_time.long_name = DIM_TIME
+    var_time.standard_name = DIM_TIME
+    time_start = DATE_BASELINE.strftime(DATE_FORMAT)
+    var_time.units = "hours since %s" % time_start
--- ozflux-lpjg/ozflux_parallel.py
+++ ozflux-lpjg/ozflux_parallel.py
@@ -3,213 +3,213 @@ import multiprocessing, multiprocessing.connection
 import ozflux_logging
 
 class Task:
-	"""
-	Base class for runnable classes.
-	"""
-	def exec(self, pcb: Callable[[float], None]):
-		pass
+    """
+    Base class for runnable classes.
+    """
+    def exec(self, pcb: Callable[[float], None]):
+        pass
 
 class _Job(multiprocessing.Process):
-	"""
-	Represents a parallel job.
-	"""
-	def __init__(self, id: int, weight: int
-	    	, task: Task
-	      	, progress_reader: multiprocessing.connection.Connection
-	    	, progress_writer: multiprocessing.connection.Connection):
-		"""
-		Create a new _Job instance.
+    """
+    Represents a parallel job.
+    """
+    def __init__(self, id: int, weight: int
+            , task: Task
+              , progress_reader: multiprocessing.connection.Connection
+            , progress_writer: multiprocessing.connection.Connection):
+        """
+        Create a new _Job instance.
 
-		@param id: Unique ID assigned to this job (used for progress reporting).
-		@param weight: Weighting of this job's progress relative to other jobs'.
-		@param task: The function to be called which performs work.
-		@param progres_reader: Read connection to the subprocess' stdout pipe.
-		@param progres_writer: Write connection to the subprocess' stdout pipe.
-		"""
-		multiprocessing.Process.__init__(self)
-		self.id = id
-		self.weight = weight
-		self.task = task
-		self.progress_reader = progress_reader
-		self.progress_writer = progress_writer
-		self.progress = 0.0
+        @param id: Unique ID assigned to this job (used for progress reporting).
+        @param weight: Weighting of this job's progress relative to other jobs'.
+        @param task: The function to be called which performs work.
+        @param progres_reader: Read connection to the subprocess' stdout pipe.
+        @param progres_writer: Write connection to the subprocess' stdout pipe.
+        """
+        multiprocessing.Process.__init__(self)
+        self.id = id
+        self.weight = weight
+        self.task = task
+        self.progress_reader = progress_reader
+        self.progress_writer = progress_writer
+        self.progress = 0.0
 
-	def _progress_local(self, progress: float, start: float,
-					 	total_weight: float):
-		"""
-		Progress reporting function used when the job is run locally.
+    def _progress_local(self, progress: float, start: float,
+                         total_weight: float):
+        """
+        Progress reporting function used when the job is run locally.
 
-		@param progress: The new progress.
-		@param start: The weight of all jobs completed before this one.
-		@param total_weight: The total weight of all jobs.
-		"""
-		self.progress = progress
-		overall = (start + self.weight * progress) / total_weight
-		ozflux_logging.log_progress(overall)
+        @param progress: The new progress.
+        @param start: The weight of all jobs completed before this one.
+        @param total_weight: The total weight of all jobs.
+        """
+        self.progress = progress
+        overall = (start + self.weight * progress) / total_weight
+        ozflux_logging.log_progress(overall)
 
-	def run(self):
-		# Do main processing.
-		self.task.exec(lambda p: self.progress_writer.send( (p, self.id) ))
+    def run(self):
+        # Do main processing.
+        self.task.exec(lambda p: self.progress_writer.send( (p, self.id) ))
 
-		# Close progress reporter pipe.
-		self.progress_writer.close()
+        # Close progress reporter pipe.
+        self.progress_writer.close()
 
-	def run_local(self, start: float, total_weight: float):
-		"""
-		Run this job on the current thread (ie not in a separate process).
+    def run_local(self, start: float, total_weight: float):
+        """
+        Run this job on the current thread (ie not in a separate process).
 
-		@param start: Total weight of all jobs already finished.
-		@param total_weight: Total weight of all jobs.
-		"""
-		self.task.exec(lambda p: self._progress_local(p, start, total_weight))
+        @param start: Total weight of all jobs already finished.
+        @param total_weight: Total weight of all jobs.
+        """
+        self.task.exec(lambda p: self._progress_local(p, start, total_weight))
 
 class JobManager:
-	def __init__(self):
-		"""
-		Create a new JobManager.
-		"""
-		# # True iff jobs are allowed to run in parallel. False otherwise.
-		# self.allow_parallel = allow_parallel
+    def __init__(self):
+        """
+        Create a new JobManager.
+        """
+        # # True iff jobs are allowed to run in parallel. False otherwise.
+        # self.allow_parallel = allow_parallel
 
-		# Total weight of all jobs. Access to this is controlled by _weights_lock.
-		self._total_weight: int = 0
+        # Total weight of all jobs. Access to this is controlled by _weights_lock.
+        self._total_weight: int = 0
 
-		# List of all submitted jobs (not running jobs!).
-		self._jobs: list[_Job] = []
+        # List of all submitted jobs (not running jobs!).
+        self._jobs: list[_Job] = []
 
-		self._lock = multiprocessing.BoundedSemaphore(1)
+        self._lock = multiprocessing.BoundedSemaphore(1)
 
-	def _progress_reporter(self, progress: float, id: int):
-		"""
-		This function is called by the wait() function when a progress report is
-		received from one of the child processes.
+    def _progress_reporter(self, progress: float, id: int):
+        """
+        This function is called by the wait() function when a progress report is
+        received from one of the child processes.
 
-		@param progress: Progress of this job in range [0, 1].
-		@param id: ID of the job which has reported its progress.
-		"""
+        @param progress: Progress of this job in range [0, 1].
+        @param id: ID of the job which has reported its progress.
+        """
 
-		job = self._jobs[id]
+        job = self._jobs[id]
 
-		weight = job.weight / self._total_weight
+        weight = job.weight / self._total_weight
 
-		job.progress = weight * progress
+        job.progress = weight * progress
 
-		# No need to check if in parallel mode, as the mutex is easily
-		# obtained when running in serial mode.
-		aggregate_progress = sum([j.progress for j in self._jobs])
-		ozflux_logging.log_progress(aggregate_progress)
+        # No need to check if in parallel mode, as the mutex is easily
+        # obtained when running in serial mode.
+        aggregate_progress = sum([j.progress for j in self._jobs])
+        ozflux_logging.log_progress(aggregate_progress)
 
-	def add_job(self, task: Task, weight: int = 1):
-		"""
-		Register a job with the job manager. A job can be any function which
-		reports its progress.
+    def add_job(self, task: Task, weight: int = 1):
+        """
+        Register a job with the job manager. A job can be any function which
+        reports its progress.
 
-		No progress reporting will occur until wait() is called.
+        No progress reporting will occur until wait() is called.
 
-		@param task: The job's execution function. This is any function which
-					 reports progress via a callable argument.
-		@param weight: Absolute weight for this job. Higher value means progress
-					in this job counts for proportionately more out of the
-					aggregate progress of all jobs. The way that the weight is
-					calculated should be consistent over all jobs, and it must
-					be positive.
-		"""
-		with self._lock:
-			# Get a job ID.
-			job_id = len(self._jobs)
+        @param task: The job's execution function. This is any function which
+                     reports progress via a callable argument.
+        @param weight: Absolute weight for this job. Higher value means progress
+                    in this job counts for proportionately more out of the
+                    aggregate progress of all jobs. The way that the weight is
+                    calculated should be consistent over all jobs, and it must
+                    be positive.
+        """
+        with self._lock:
+            # Get a job ID.
+            job_id = len(self._jobs)
 
-			# Update job weights. Note that the newly-added job weight should have
-			# index job_id.
-			self._total_weight += weight
+            # Update job weights. Note that the newly-added job weight should have
+            # index job_id.
+            self._total_weight += weight
 
-			# Create a pipe for 1-way communication (progress reporting).
-			reader, writer = multiprocessing.connection.Pipe(duplex = False)
+            # Create a pipe for 1-way communication (progress reporting).
+            reader, writer = multiprocessing.connection.Pipe(duplex = False)
 
-			# Start the process.
-			job = _Job(job_id, weight, task, reader, writer)
+            # Start the process.
+            job = _Job(job_id, weight, task, reader, writer)
 
-			# Store the process handle for later use.
-			self._jobs.append(job)
+            # Store the process handle for later use.
+            self._jobs.append(job)
 
-	def _get_num_running_jobs(self):
-		"""
-		Get the number of currently running jobs.
-		"""
-		return len([j for j in self._jobs if j.is_alive()])
+    def _get_num_running_jobs(self):
+        """
+        Get the number of currently running jobs.
+        """
+        return len([j for j in self._jobs if j.is_alive()])
 
-	def run_parallel(self, max_para: int = multiprocessing.cpu_count()):
-		"""
-		Run all jobs in parallel and wait for them to finish.
-		@param max_para: Maximum number of jobs to run in parallel. Defaults to logical CPU count.
-		"""
-		with self._lock:
-			for job in self._jobs:
-				# If number of running jobs exceeds available number of CPUs,
-				# wait for one job to finish.
-				num_running = self._get_num_running_jobs()
-				if num_running >= max_para:
-					self._wait_until(lambda: self._get_num_running_jobs() < max_para)
+    def run_parallel(self, max_para: int = multiprocessing.cpu_count()):
+        """
+        Run all jobs in parallel and wait for them to finish.
+        @param max_para: Maximum number of jobs to run in parallel. Defaults to logical CPU count.
+        """
+        with self._lock:
+            for job in self._jobs:
+                # If number of running jobs exceeds available number of CPUs,
+                # wait for one job to finish.
+                num_running = self._get_num_running_jobs()
+                if num_running >= max_para:
+                    self._wait_until(lambda: self._get_num_running_jobs() < max_para)
 
-				job.start()
+                job.start()
 
-				# Close the writable end of the pipe now, to be sure that p is
-				# the only process which owns a handle for it. This ensures that
-				# when p closes its handle for the writable end, wait() will
-				# promptly report the readable end as being ready.
-				job.progress_writer.close()
+                # Close the writable end of the pipe now, to be sure that p is
+                # the only process which owns a handle for it. This ensures that
+                # when p closes its handle for the writable end, wait() will
+                # promptly report the readable end as being ready.
+                job.progress_writer.close()
 
-				# Wait until job starts, to prevent race conditions.
-				self._wait_until(lambda: job.is_alive())
+                # Wait until job starts, to prevent race conditions.
+                self._wait_until(lambda: job.is_alive())
 
-			# Wait until all jobs are finished.
-			self._wait_until()
-			for process in self._jobs:
-				process.join()
+            # Wait until all jobs are finished.
+            self._wait_until()
+            for process in self._jobs:
+                process.join()
 
-			# If any job failed, raise an exception.
-			# TODO: is it possible to include error context (ie exception)?
-			for job in self._jobs:
-				if not (job.exitcode == 0 or job.exitcode is None):
-					raise RuntimeError(f"Job {job} exited with non-zero exit code")
+            # If any job failed, raise an exception.
+            # TODO: is it possible to include error context (ie exception)?
+            for job in self._jobs:
+                if not (job.exitcode == 0 or job.exitcode is None):
+                    raise RuntimeError(f"Job {job} exited with non-zero exit code")
 
-	def run_single_threaded(self):
-		"""
-		Run all jobs one at a time, in the current thread, and wait for them to
-		finish.
-		"""
-		with self._lock:
-			cum_weight = 0
-			for job in self._jobs:
-				# The pipe connection is not required.
-				job.progress_reader.close()
-				job.progress_writer.close()
+    def run_single_threaded(self):
+        """
+        Run all jobs one at a time, in the current thread, and wait for them to
+        finish.
+        """
+        with self._lock:
+            cum_weight = 0
+            for job in self._jobs:
+                # The pipe connection is not required.
+                job.progress_reader.close()
+                job.progress_writer.close()
 
-				job.run_local(cum_weight, self._total_weight)
-				if not (job.exitcode == 0 or job.exitcode is None):
-					raise RuntimeError(f"Job {job} exited with non-zero exit code")
-				cum_weight += job.weight
+                job.run_local(cum_weight, self._total_weight)
+                if not (job.exitcode == 0 or job.exitcode is None):
+                    raise RuntimeError(f"Job {job} exited with non-zero exit code")
+                cum_weight += job.weight
 
-	def _wait_until(self, condition: Callable[[], bool] = lambda: False):
-		"""
-		Wait for all parallel jobs to finish running, or the given condition
-		returns true. Note that no progress messages will be written until this
-		is called.
+    def _wait_until(self, condition: Callable[[], bool] = lambda: False):
+        """
+        Wait for all parallel jobs to finish running, or the given condition
+        returns true. Note that no progress messages will be written until this
+        is called.
 
-		@param condition: Optional function which returns a bool. If this
-		function returns true, waiting will cease. If no condition is given,
-		this function will wait until all jobs have finished.
-		"""
-		readers = [j.progress_reader for j in self._jobs if j.is_alive()]
-		while readers:
-			for reader in readers:
-				try:
-					if condition():
-						return
-					msg = None
-					if reader.poll():
-						msg = reader.recv()
-						(progress, process_index) = msg
-						self._progress_reporter(progress, process_index)
-				except EOFError:
-					readers.remove(reader)
+        @param condition: Optional function which returns a bool. If this
+        function returns true, waiting will cease. If no condition is given,
+        this function will wait until all jobs have finished.
+        """
+        readers = [j.progress_reader for j in self._jobs if j.is_alive()]
+        while readers:
+            for reader in readers:
+                try:
+                    if condition():
+                        return
+                    msg = None
+                    if reader.poll():
+                        msg = reader.recv()
+                        (progress, process_index) = msg
+                        self._progress_reporter(progress, process_index)
+                except EOFError:
+                    readers.remove(reader)
 
